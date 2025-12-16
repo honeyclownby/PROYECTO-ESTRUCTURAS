ae4d9e8f278b004d1e4eed5119cc73fa740613f7
Oscar-Cast <zs24013043@estudiantes.uv.mx>
2025-12-15 23:01:27 -0600

    Add age and seniority calculations to Contacto class

--- Contacto.py
+++ Contacto.py
@@ -16,3 +16,25 @@ class Contacto:
         self.fecha_ingreso = fecha_ingreso
         
         self.id = generarId(matricula)
+        self.edad = calcularEdad(fecha_nacimiento )
+        self.antiguedad = calcularAntiguedad(fecha_ingreso)
+        self.fechaRegistro = FechaRegistroActu(%d/%m/%Y)
+        self.tiempoActualizacion = "" 
+        
+        def mostrarInformacion(self):
+            info = f"ID: {self.id}\n"
+            info += f"Nombres: {self.nombres} {self.apellido_paterno} {self.apellido_materno}\n"
+            info += f"Dirección: {self.direccion}, {self.ciudad}, {self.estado}\n"
+            info += f"Fecha de Nacimiento: {self.fecha_nacimiento} (Edad: {self.edad})\n"
+            info += f"Teléfono: {self.telefono}\n"
+            info += f"Correo Personal: {self.correo_personal}\n"
+            info += f"Matrícula: {self.matricula}\n"
+            info += f"Correo Institucional: {self.correo_institucional}\n"
+            info += f"Facultad: {self.facultad}\n"
+            info += f"Programa Educativo: {self.programa_educativo}\n"
+            info += f"Fecha de Ingreso: {self.fecha_ingreso} (Antigüedad: {self.antiguedad})\n"
+            info += f"Fecha de Registro: {self.fechaRegistro}\n"
+            info += f"Tiempo desde la última actualización: {self.tiempoActualizacion}\n"
+            return info
+
+        
