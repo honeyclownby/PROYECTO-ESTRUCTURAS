f91e35e994f304216a472f2a2a50c63bebbc2737
Oscar-Cast <zs24013043@estudiantes.uv.mx>
2025-12-16 02:14:51 +0000

    inicio clase contacto

--- Contacto.py
+++ Contacto.py
@@ -1,1 +1,18 @@
-
+class Contacto:
+    def __init__(self, nombres, apellido_paterno, apellido_materno, direccion, estado, ciudad, fecha_nacimiento, telefono, correo_personal, matricula, correo_institucional, facultad, programa_educativo, fecha_ingreso):
+        self.nombres = nombres
+        self.apellido_paterno = apellido_paterno
+        self.apellido_materno = apellido_materno
+        self.direccion = direccion
+        self.estado = estado
+        self.ciudad = ciudad
+        self.fecha_nacimiento = fecha_nacimiento
+        self.telefono = telefono
+        self.correo_personal = correo_personal
+        self.matricula = matricula
+        self.correo_institucional = correo_institucional
+        self.facultad = facultad
+        self.programa_educativo = programa_educativo
+        self.fecha_ingreso = fecha_ingreso
+        
+        self.id = generarId(matricula)
